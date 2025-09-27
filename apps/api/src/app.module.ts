63c309e97b3c520baa63f6ddac404bbb417f4a60
phibuinek <phibuinek@gmail.com>
2025-09-27 19:09:29 +0700

    2nd day

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -6,6 +6,11 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
 import { PostModule } from './post/post.module';
+import { UserModule } from './user/user.module';
+import { CommentModule } from './comment/comment.module';
+import { TagModule } from './tag/tag.module';
+import { LikeModule } from './like/like.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -15,6 +20,11 @@ import { PostModule } from './post/post.module';
       autoSchemaFile: join(process.cwd(), 'src/graphql/schema.gql'),
     }),
     PostModule,
+    UserModule,
+    CommentModule,
+    TagModule,
+    LikeModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
