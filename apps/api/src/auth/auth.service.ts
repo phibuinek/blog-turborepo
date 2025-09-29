9f06faef2d5ee333943b7c1aaecae6c95f053e89
phibuinek <phibuinek@gmail.com>
2025-09-29 12:38:01 +0700

    3rd day

--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -2,10 +2,16 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { SignInInput } from './dto/signin.input';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { verify } from 'argon2';
+import { JwtService } from '@nestjs/jwt';
+import { AuthJwtPayload } from './types/auth-jwtPayload';
+import { User } from 'generated/prisma';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
   async validateLocalUser({ email, password }: SignInInput) {
     const user = await this.prisma.user.findUnique({
       where: {
@@ -13,7 +19,7 @@ export class AuthService {
       },
     });
     if (!user) throw new UnauthorizedException('User Not Found');
-    
+
     if (!user.password) {
       throw new Error('User does not have a password');
     }
@@ -23,5 +29,32 @@ export class AuthService {
       throw new UnauthorizedException('Invalid Credentials!');
 
     return user;
+  }
+
+  async generateToken(userId: number) {
+    const payload: AuthJwtPayload = { sub: userId };
+    const accessToken = await this.jwtService.signAsync(payload);
+    return { accessToken };
+  }
+
+  async login(user: User) {
+    const { accessToken } = await this.generateToken(user.id);
+    return {
+      id: user.id,
+      name: user.name,
+      avatar: user.avatar,
+      accessToken,
+    };
+  }
+
+  async validateJwtUser(userId: number) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+    if (!user) throw new UnauthorizedException('User not found!');
+    const currentUser = { id: user.id };
+    return currentUser;
   }
 }
